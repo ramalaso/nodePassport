99bbdd7f1aa87feca39de3370ec580ac3e804166
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-10-18 17:35:11 -0400

    Added welcome page ejs

---
+++ routes/users.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const router = express.Router()
+
+
+//Login
+router.get('/login', (req, res)=>{
+    res.send('Login')
+})
+
+router.get('/register', (req, res)=>{
+    res.send('Register')
+})
+
+module.exports = router;
